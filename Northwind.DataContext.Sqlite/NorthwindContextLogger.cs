4138bf236b8d37032589d996e14a5da7a2fd02d4
amarchi <andrea.marchi@mjinsurance.com>
2025-02-14 11:22:02 -0500

    Restored Northwind.DataContext.Sqlite and Northwind.EntityModels.Sqlite projects

---
+++ Northwind.DataContext.Sqlite/NorthwindContextLogger.cs
@@ -0,0 +1,32 @@
+﻿using static System.Environment;
+
+namespace Northwind.EntityModels;
+
+public class NorthwindContextLogger
+{
+    public const string logFolder = "book-logs";
+
+    public static void WriteLine(string message)
+    {
+        if(!Directory.Exists(logFolder))
+        {
+            Directory.CreateDirectory(logFolder);
+        }
+        string folder = Path.Combine(GetFolderPath(SpecialFolder.DesktopDirectory), logFolder);
+        if (!Directory.Exists(folder))
+        {
+            Directory.CreateDirectory(folder);
+        }
+        string dateTimeStamp = DateTime.Now.ToString("yyyyMMdd_HHmmss");
+        string path = Path.Combine(folder, $"northwindlog-{dateTimeStamp}.txt");
+
+
+        using(StreamWriter sw = File.AppendText(path))
+        {
+            sw.WriteLine(message);
+        }
+
+
+
+    }
+}
